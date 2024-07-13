04c0c02b10b24f4e3b84f3450410cf0491a0394c
Radiniks <radiniks@gmail.com>
2024-07-13 09:07:50 +0200

    Update new_code_test.js

--- new_code_test.js
+++ new_code_test.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     console.log("Script loaded successfully!");
 
       $.ajax({
-                url: 'https://your-infinityfree-domain.com/DFproject/cf_configurator.php',
+                url: 'https://radinikstemp.great-site.net/DFproject/cf_configurator.php',
                 method: 'GET',
                 data: { action: 'step1' },
                 success: function(response) {
